25e0d855d0901a3c3e420d2a22e13fab4e6801ea
Ngô Văn Minh <126498371+ngominh04@users.noreply.github.com>
2023-10-23 09:41:46 +0700

    Delete service directory

--- service/IService.java
+++
@@ -1,9 +0,0 @@
-package vn.com.devmaster.service.managermaterial.service;
-
-import vn.com.devmaster.service.managermaterial.domain.Product;
-
-import java.util.Optional;
-
-public interface IService {
-    Product finById(Integer id);
-}
