d6786c402e25c0b53874b7e3ce698e4e3a4f4391
Ngô Văn Minh <126498371+ngominh04@users.noreply.github.com>
2023-10-23 09:41:11 +0700

    Delete projecttion directory

--- projecttion/IPayment_method.java
+++
@@ -1,8 +0,0 @@
-package vn.com.devmaster.service.managermaterial.projecttion;
-
-public interface IPayment_method {
-    String getId();
-    String getName();
-    String getNotes();
-
-}
--- projecttion/IProductImage.java
+++
@@ -1,8 +0,0 @@
-package vn.com.devmaster.service.managermaterial.projecttion;
-
-public interface IProductImage {
-    String getId();
-    String getName();
-    String getNotes();
-    String getUrl();
-}
